c6a03374139a457e3790372202fdc7f3517ad408
omarmushtaha2525gmail.com <omar200234>
2024-10-02 11:01:23 +0300

    build Lottie_Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie_animation_app/lottie_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: LottieScreen(),
     );
   }
 }
